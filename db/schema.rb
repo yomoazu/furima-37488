6129af897538df4e8c0ba3af58e305b4f3b0bdce
yomoazu <0806.kimiko@gmail.com>
2022-06-28 11:08:42 +0900

    テストに成功する画像作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_075132) do
+ActiveRecord::Schema.define(version: 2022_06_27_082720) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "product_name", null: false
+    t.text "description_of_item", null: false
+    t.integer "category_id", null: false
+    t.integer "product_condition_id", null: false
+    t.integer "delivery_charge_id", null: false
+    t.integer "area_id", null: false
+    t.integer "shipping_day_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_items_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_items_on_reset_password_token", unique: true
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    # ユーザー新規登録についてのテストコードを記述します
+    it 'nicknameが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します
+    end
+    it 'emailが空では登録できない' do
+    end
+      # emailが空では登録できないテストコードを記述します
+    it 'encrypted_passwordが空では登録できない' do
+    end
+    it 'first_nameが空では登録できない' do
+    end
+    it 'first_name_furiganaが空では登録できない' do
+    end
+    it 'last_nameが空では登録できない' do
+    end
+    it 'last_name_furiganaが空では登録できない' do  
+    end
+    it 'date_of_bathが空では登録できない' do 
+    end  
+  end
+end 
