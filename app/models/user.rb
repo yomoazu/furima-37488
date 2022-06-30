608614363e8a0db5fdabf2e9372790fbd0d2371f
yomoazu <0806.kimiko@gmail.com>
2022-06-30 12:53:23 +0900

    修正6.30

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, presence: true
+
+  
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
 
