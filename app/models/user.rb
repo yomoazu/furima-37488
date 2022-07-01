ea3e2c63173334b3c50a83b94be9e05904a09b1d
yomoazu <0806.kimiko@gmail.com>
2022-07-01 14:15:32 +0900

    修正再再

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,10 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
 
-  validates :first_name,format:{with:/\A[ぁ-んァ-ヶ一-龥々ー]/+\z,message:"全角ひらがな・カタカナ・漢字でご入力下さい。"}
-  validates :last_name,format:{with:/\A[ぁ-んァ-ヶ一-龥々ー]/+\z,message:"全角ひらがな・カタカナ・漢字でご入力下さい。"}
-  validates :first_name,:first_name_furigana,format:{with://\A[ァ-ヶー－]+\z/,message:"全角カタカナでご入力下さい。"}
-  validates :first_name,:last_name_furigana,format:{with://\A[ァ-ヶー－]+\z/,message:"全角カタカナでご入力下さい。"}
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角ひらがな・カタカナ・漢字でご入力下さい'}
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角ひらがな・カタカナ・漢字でご入力下さい'}
+  validates :first_name, :first_name_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナでご入力下さい"}
+  validates :first_name, :last_name_furigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナでご入力下さい"}
   
   validates :first_name, presence: true
   validates :first_name_furigana, presence: true
