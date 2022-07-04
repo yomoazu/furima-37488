b431e254ae1d4b7fc1132109c5f837758c334e16
yomoazu <0806.kimiko@gmail.com>
2022-07-04 16:34:13 +0900

    テストコード作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
     
+    product_name              {'444'}
+    description_of_item       {'444'}
+    category_id               {'10'}
+    product_condition_id      {'3'}
+    delivery_charge_id        {'2'}
+    area_id                   {'4'}
+    shipping_day_id           {'3'}
+    praice                    {'444444444'}
+    
+      
+
+
+
   end
 end
