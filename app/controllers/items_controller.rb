63838e0227dc131c64eb782d87a85cbb43a551e9
yomoazu <0806.kimiko@gmail.com>
2022-07-05 17:22:37 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,22 +1,37 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    # @items = Item.order("created_at DESC")
+    
+    @items = Item.order("created_at DESC")
   end
 
   def new
+    if user_signed_in?
     @item = Item.new
+    else
+      redirect_to user_session_path(@item_id)
+   end
   end
 
  
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to items_path
+      #redirect_to root_path
     else
       render :new
+      #render :new
     end
-    end
+  end
+
+  def show
+   def set_product
+    @item = Item.find(params[:id])
+    #@product = Product.find(params[:id])
+   end
+
+  end
 
   private
   def item_params
