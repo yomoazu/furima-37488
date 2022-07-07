fb5962d19d374b6e709a1ed453d469f788f5b1d2
yomoazu <0806.kimiko@gmail.com>
2022-07-07 17:16:23 +0900

    商品詳細ページ修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,9 @@
 class ItemsController < ApplicationController
   #before_action :set_product, only: [:show, :smove_to_index]
   before_action :set_item,only: [:edit, :show, :update]
-  before_action :move_to_index,except: [:new, :create, :edit, :update]
-
+  before_action :move_to_index,except: [:index, :show]
+  before_action :ensure_current_user,except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     
     @items = Item.order("created_at DESC")
@@ -57,7 +58,7 @@ class ItemsController < ApplicationController
   end
 
   def ensure_current_user
-    item = Item.find(item[:id])
+    @item = Item.find(params[:id])
     if @item.user_id != current_user.id
       redirect_to action: :index
     end
@@ -71,5 +72,6 @@ class ItemsController < ApplicationController
       redirect_to action: :index
     end
   end
+  # authentictae_user!
 
 end
