620ef8e25a11ff571a174ae5fcf95118bfce41b8
yomoazu <0806.kimiko@gmail.com>
2022-07-14 21:33:30 +0900

    修正完了

--- db/migrate/20220709111604_create_orders.rb
+++ db/migrate/20220709111604_create_orders.rb
@@ -9,20 +9,3 @@ class CreateOrders < ActiveRecord::Migration[6.0]
   end
 end
 
-# | Column                | Type       | Options                        |
-# | ----------------------| ---------- | ------------------------------ |
-# | user                  | references | null: false, foreign_key: true |
-# | item                  | references | null: false, foreign_key: true |
-
-
-
-# | Column                | Type       | Options                        |
-# | ----------------------| ---------- | -------------------------------|
-# | post_code             | string     | null: false                    |
-# | area_id               | integer    | null: false                    |
-# | municipality          | string     | null: false                    |
-# | address               | string     | null: false                    |
-# | building_name         | string     |                                |
-# | phone_number          | string     | null: false                    |
-# | order                 | references | null: false, foreign_key: true |
-
