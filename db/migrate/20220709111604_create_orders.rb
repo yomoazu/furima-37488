e3a14e39cad43e1005dfb416e2e24d26533a650c
yomoazu <0806.kimiko@gmail.com>
2022-07-15 14:14:24 +0900

    Update 20220709111604_create_orders.rb

--- db/migrate/20220709111604_create_orders.rb
+++ db/migrate/20220709111604_create_orders.rb
@@ -1,6 +1,6 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
-    create_table :order do |t|
+    create_table :orders do |t|
  
         t.references :user, null: false, foreign_key: true
         t.references :item, null: false, foreign_key: true
